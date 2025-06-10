e74115cb970d4b47bd1814a4db88e43041671994
Blake Rooker <211597299+ColourFour@users.noreply.github.com>
2025-06-10 13:46:22 +0800

    made the number of boxes dynamic but need to adjust so that boxes adjust properly (issue with width/height now)

--- eas.js
+++ eas.js
@@ -1,10 +1,21 @@
-let boxNumber = 16;
-
+const input = document.getElementById("html-boxNumber");
 const container = document.querySelector(".container");
 
-for(let i = 0; i< boxNumber; i++){
-const interactBox = document.createElement("div");
-interactBox.setAttribute("id", `boxPosition${i}`)
-interactBox.setAttribute("class", `colorBox`)
-container.appendChild(interactBox);
-}
+input.addEventListener("input", () => {
+  const boxNumber = Number(input.value);
+
+  container.innerHTML = '';
+
+  for (let i = 0; i < boxNumber; i++) {
+    const interactBox = document.createElement("div");
+    interactBox.setAttribute("id", `boxPosition${i}`);
+    interactBox.setAttribute("class", "colorBox");
+    container.appendChild(interactBox);
+  }
+  input.addEventListener("input", () => {
+  const value = Number(input.value);
+  if (value > 0) {
+    document.documentElement.style.setProperty('--box-divisor', value);
+  }
+});
+});
