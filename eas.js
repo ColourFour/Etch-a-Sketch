1fec07eddc8af70cadb9e2026f5057c29c6fb582
Blake Rooker <211597299+ColourFour@users.noreply.github.com>
2025-06-16 14:26:52 +0800

    edits for clarity

--- eas.js
+++ eas.js
@@ -6,16 +6,22 @@ input.addEventListener("input", () => {
 
   container.innerHTML = '';
 
-  for (let i = 0; i < boxNumber; i++) {
+  for (let i = 0; i < boxNumber * boxNumber; i++) {
     const interactBox = document.createElement("div");
     interactBox.setAttribute("id", `boxPosition${i}`);
     interactBox.setAttribute("class", "colorBox");
     container.appendChild(interactBox);
   }
+  
   input.addEventListener("input", () => {
   const value = Number(input.value);
   if (value > 0) {
     document.documentElement.style.setProperty('--box-divisor', value);
   }
+  
 });
+});
+
+input.addEventListener("hover",() =>{
+  interactBox.setAttribute("class", "filled");
 });
