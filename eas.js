992287d83dff0af91e25e35bf3c88e40cfe069f7
Blake Rooker <211597299+ColourFour@users.noreply.github.com>
2025-07-10 21:36:55 +0800

    created working javascript -> need to imprpove css

--- eas.js
+++ eas.js
@@ -1,2 +1,21 @@
-let gridSize = document.getElementById("grid-value");
-console.log(`${gridSize}`);
+const gridSize = document.getElementById("grid-value");
+const container = document.getElementById("container");
+
+
+function createGrid(){
+    container.innerHTML = "";
+    const value = Number(gridSize.value); 
+    if (isNaN(value)) return;
+
+    const squareGrid = value ** 2;
+
+    for (let i = 0; i < squareGrid; i++) {
+        const newDiv = document.createElement("div");
+        newDiv.classList.add("grid-square");
+        container.appendChild(newDiv);
+    }
+}
+
+gridSize.addEventListener("input", createGrid);
+
+
