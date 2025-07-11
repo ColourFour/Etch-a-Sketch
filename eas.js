84447049d95b0da56c72622f7b58fbaaa8a4c7c6
Blake Rooker <211597299+ColourFour@users.noreply.github.com>
2025-07-11 14:03:32 +0800

    completed project

--- eas.js
+++ eas.js
@@ -8,12 +8,25 @@ function createGrid(){
     if (isNaN(value)) return;
 
     const squareGrid = value ** 2;
+    const boxSize = 600 / value;
+    container.style.setProperty('--square-size', `${boxSize}px`);
 
     for (let i = 0; i < squareGrid; i++) {
         const newDiv = document.createElement("div");
         newDiv.classList.add("grid-square");
         container.appendChild(newDiv);
+        newDiv.addEventListener("mouseenter", () => {
+        newDiv.classList.toggle("active");
+        newDiv.style.backgroundColor = randomColor(); 
+});
     }
+}
+
+const randomColor = function getRandomColor() {
+  const r = Math.floor(Math.random() * 256); 
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+  return `rgb(${r}, ${g}, ${b})`;
 }
 
 gridSize.addEventListener("input", createGrid);
